ce60dc2d900ea37ae3642eda1fe72e149df740fc
Max Huang <github@maxhuang.nz>
2018-08-03 14:53:48 +1200

    Added .travis.yml file for Python

--- etude01/HVTrees.py
+++ etude01/HVTrees.py
@@ -1,5 +1,3 @@
-# import numpy as np
-
 import matplotlib.pyplot as plt
 import sys
 
@@ -17,12 +15,16 @@ def drawLine(x, y, len, order):
         # len *= factor
         orderCount += 1
         print("mod", count%2)
-        if count % 2 == 0:
+        if count % 2 == 0:  # Decides whether the line should be vertical or horizontal.
             count += 1
             drawHorizontal = True
             print(">>", len)
             plt.plot([(x-len/2),(x+len/2)], [y,y], 'r-')
             print('true')
+            #? what if I tried drawing the two lines at the ends of this line instead??
+            #? plt.plot([x-len/2,x-len/2], [y-len/2,y+len/2], 'b-')
+            #? plt.plot([x+len/2,x+len/2], [y-len/2,y+len/2], 'g-')
+            #? Issue - It's very messy!
             drawLine(x+len/2, y, len*factor, order)
             print("next x")
             drawLine(x-len/2, y, len*factor, order)
@@ -39,6 +41,9 @@ def drawLine(x, y, len, order):
         print("else")
         return
     return
+
+# Once recursion has hit the order limit, need to recurse back
+# to draw the rest of the lines.
 
 
 
